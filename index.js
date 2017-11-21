5187abcb41c562ad39f0cacc42b1d84bd450e32a
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-11-21 20:01:10 +0100

    Replace charset from input
    Regex is based on https://github.com/frankkanis/charset-parser/blob/master/index.js#L4
    
    Another reference: https://github.com/iciclize/html-charset/blob/master/index.js#L18

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-'use strict';
+'use strict'
 
-var jschardet = require('jschardet');
-var charset = require('charset');
-var iconv = require('iconv-lite');
+var jschardet = require('jschardet')
+var iconv = require('iconv-lite')
+var charset = require('charset')
 
-module.exports = ensureUTF8;
+var charsetRegex = /charset=["]*([^>"\s]+)/i
 
-function ensureUTF8(binaryBuffer, contentType) {
-  var encoding = detectEncoding(binaryBuffer, contentType);
+module.exports = function ensureUTF8 (binaryBuffer, contentType) {
+  var encoding = detectEncoding(binaryBuffer, contentType)
+  if (encoding === 'utf8') return binaryBuffer.toString('utf8')
 
-  if (encoding === 'utf8') return binaryBuffer.toString('utf8');
-
-  return iconv.decode(binaryBuffer, encoding);
+  return iconv
+    .decode(binaryBuffer, encoding)
+    .replace(charsetRegex, 'utf-8')
 }
 
-function detectEncoding(content, contentType) {
-  var byMetaAndContentType = charset({'content-type': contentType}, content);
-  if (byMetaAndContentType) return byMetaAndContentType;
-
-  var byGuessing = jschardet.detect(content);
-  if (byGuessing && byGuessing.encoding) return byGuessing.encoding;
-
-  return 'utf8';
+function detectEncoding (content, contentType) {
+  return charset({'content-type': contentType}, content) || jschardet.detect(content) || 'utf8'
 }
